b018629743d944fffffe1e47e5c1224c9f1a13a3
elia605 <elia.galkova@yandex.ru>
2021-06-06 00:13:25 +0800

    web.php with routes listing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,15 +13,40 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes(['verify' => true]);
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'welcome'])->name('/');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('auth/vk', [App\Http\Controllers\Auth\AuthController::class, 'redirectToProvider'])->name('login.vk');
+Route::get('auth/vk/callback', [App\Http\Controllers\Auth\AuthController::class, 'handleProviderCallback']);
+
+Route::get('/members', [\App\Http\Controllers\HomeController::class, 'members'])->name('members');
+
+Route::get('/actions', [\App\Http\Controllers\HomeController::class, 'actions'])->name('actions');
+Route::get('/action/{action}', [\App\Http\Controllers\HomeController::class, 'actionView'])->name('actionView');
+
+Route::get('/news/', [\App\Http\Controllers\HomeController::class, 'news'])->name('news');
+Route::get('/new/{new}', [\App\Http\Controllers\HomeController::class, 'newView'])->name('newView');
+
+Route::middleware('auth')->get('/profile', [\App\Http\Controllers\HomeController::class, 'profile'])->name('profile');
+
+
+Route::prefix('admin')->middleware('admin')->group(function() {
+    Route::get('/', [\App\Http\Controllers\Admin\AdminController::class, 'index'])->name('admin.index');
+    Route::get('/users', [\App\Http\Controllers\Admin\AdminController::class, 'users'])->name('admin.users');
+    Route::delete('/delete/{user}', [\App\Http\Controllers\Admin\AdminController::class, 'deleteUser'])->name('admin.user.delete');
 });
 
-Auth::routes();
+Route::prefix('org')->middleware('org')->group(function() {
+    Route::get('/', [\App\Http\Controllers\Org\OrgController::class, 'index'])->name('org.index');
+    Route::get('/members', [\App\Http\Controllers\Org\OrgController::class, 'members'])->name('org.members');
+    Route::get('/news', [\App\Http\Controllers\Org\OrgController::class, 'news'])->name('org.news');
+    Route::get('/actions', [\App\Http\Controllers\Org\OrgController::class, 'actions'])->name('org.actions');
+    Route::get('/requests', [\App\Http\Controllers\Org\OrgController::class, 'requests'])->name('org.requests');
+    Route::get('pdfview',array('as'=>'pdfview','uses'=>'\App\Http\Controllers\Org\OrgController@pdfview'));
+    Route::get('/documents', [\App\Http\Controllers\Org\OrgController::class, 'documents'])->name('org.documents');
+    Route::get('/carousel', [\App\Http\Controllers\Org\OrgController::class, 'carousel'])->name('org.carousel');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});

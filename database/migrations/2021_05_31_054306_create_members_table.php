252bf2b62b09802ccac02bb285406d8339a6022e
elia605 <elia.galkova@yandex.ru>
2021-06-05 11:38:19 +0800

    Members migration

---
+++ database/migrations/2021_05_31_054306_create_members_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id')->constrained('users')->nullable();
+            $table->string('FIO');
+            $table->date('birthday');
+            $table->string('role');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('group');
+            $table->boolean('notify');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}

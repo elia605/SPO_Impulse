5da884228899ba6908c223ab9f048ad6a23eba70
elia605 <elia.galkova@yandex.ru>
2021-06-06 00:05:31 +0800

    app.css additions for datepicker

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,3 +30,13 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
 });
+
+$( ".datepicker" ).datepicker({
+    format: "mm/dd/yy",
+    weekStart: 0,
+    calendarWeeks: true,
+    autoclose: true,
+    todayHighlight: true,
+    rtl: true,
+    orientation: "auto"
+});

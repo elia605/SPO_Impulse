a6b6c378f7be6abffbb6bd7db3ef14097c6a84f4
elia605 <elia.galkova@yandex.ru>
2021-06-06 00:30:17 +0800

    Kernel register middlewares for route

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\AccessToAdmin;
+use App\Http\Middleware\AccessToOrg;
+use App\Http\Middleware\Banned;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -62,5 +65,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => AccessToAdmin::class,
+        'org' => AccessToOrg::class,
+        'banned' => Banned::class,
     ];
 }
